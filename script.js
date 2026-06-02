23fc6b58abb631a11859e960dc578fd8e4526939
Kiwww17 <wimarizky17@gmail.com>
2026-06-02 14:48:36 +0700

    Major logic and apperance bug fix

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const generate = document.getElementById('generate');
 generate.onclick = () => {
 
@@ -39,7 +38,7 @@ generate.onclick = () => {
     const public = document.getElementById('public');
     const body = document.body
 
-
+    const backButton = document.getElementById('button');
     let state;
     const settings = document.getElementById('settings');
     if (
@@ -78,6 +77,8 @@ generate.onclick = () => {
         pub_kodeNamaProdiTp.textContent = kodeProdi + ' - ' + namaProdi + '  (' + tingkatPendidikan + ')'
 
         public.style.display = 'flex';
+        public.style.height = '100%';
+        public.style.width = '100%';
         settings.style.display = 'none';
         state = true;
         body.style.paddingTop = '0';
@@ -85,9 +86,10 @@ generate.onclick = () => {
         body.style.paddingLeft = '0';
         body.style.paddingRight = '0';
         body.style.height = '100vh';
-        body.style.width = '100%';
-        body.style.overflow = 'hidden';
+        body.style.width = '100vw';
+        body.style.overflow = 'visible';
         body.style.backgroundImage = 'none';
+        backButton.style.display = 'flex'
 
     }
 
@@ -121,6 +123,7 @@ button.addEventListener('click', function () {
     public.style.display = 'none';
     settings.style.display = 'flex';
     settings.style.background = 'rgba(0, 0, 0, 0.58)';
+    button.style.display='none';
 })
 
 //-----------Mobile Generate------------------------//
@@ -138,19 +141,10 @@ generateMobile.onclick = () => {
     let mbMname = document.getElementById('mb-mn').value;
 
     let tingkatPendidikan = document.querySelector('input[name="mbTingkat pendidikan"]:checked')?.value;
-
     const body = document.body;
     ///////////
-    console.log(mbNumber);
-    console.log(mbName);
-    console.log(mbDate);
-    console.log(mbMonth);
-    console.log(mbYear);
-    console.log(mbCcode);
-    console.log(mbCname);
-    console.log(mbMcode);
-    console.log(mbMname);
-    console.log(tingkatPendidikan);
+    
+    //Out//
     const mNomorP = document.getElementById('mb-pub-nomorPeserta');
     const mNamaP = document.getElementById('mb-pub-namaPeserta');
     const mTanggalP = document.getElementById('mb-pub-tglPeserta');
@@ -186,7 +180,7 @@ generateMobile.onclick = () => {
         body.style.paddingBottom = '16px';
         body.style.paddingLeft = '16px';
         body.style.paddingRight = '16px';
-        body.style.height = '100vh';
+        body.style.height = '';
         body.style.width = '100%';
         body.style.overflow = 'hidden';
         body.style.backgroundImage = 'none';
@@ -199,8 +193,7 @@ generateMobile.onclick = () => {
         setTimeout(() => {
             alert("NB : Untuk kembali ke section sebelumnya klik pada kotak berwana hijau dibagian paling bawah");
         }, 3000);
-
-    } else {
+    } else{
 
     }
 
@@ -228,115 +221,3 @@ const mbReset = document.getElementById('mb-res-btn');
 mbReset.onclick = () => {
     window.location.reload()
 }
-
-const generate = document.getElementById('generate');
-generate.onclick = () => {
-
-
-    let nomorPeserta = document.getElementById('nomorPeserta').value;
-
-    let namaPeserta = document.getElementById('namaPeserta').value;
-    namaPeserta = namaPeserta.toUpperCase()
-
-    let tglLahir = document.getElementById('tglLahir').value;
-
-    let blnLahir = document.getElementById('blnLahir').value;
-
-    let thnLahir = document.getElementById('thnLahir').value;
-
-    let kodePTN = document.getElementById('kodePTN').value;
-
-    let namaPTN = document.getElementById('namaPTN').value;
-    namaPTN = namaPTN.toUpperCase();
-
-
-    let kodeProdi = document.getElementById('kodeProdi').value;
-
-    let namaProdi = document.getElementById('namaProdi').value;
-    namaProdi = namaProdi.toUpperCase();
-
-
-    let tingkatPendidikan = document.querySelector('input[name="Tingkat pendidikan"]:checked')?.value;
-
-
-    //////////-------------------------------------//////
-    const pub_nomorPeserta = document.getElementById('pub-nomorPeserta');
-    const pub_namaPeserta = document.getElementById('pub-namaPeserta');
-    const pub_tglPeserta = document.getElementById('pub-tglPeserta');
-    const pub_kodeNamaPTN = document.getElementById('pub-kodeNamaPTN');
-    const pub_kodeNamaProdiTp = document.getElementById('pub-kodeNamaProdiTp');
-
-    const public = document.getElementById('public');
-    const body = document.body
-
-
-    let state;
-    const settings = document.getElementById('settings');
-    if (
-        tingkatPendidikan == undefined || nomorPeserta == "" || namaPeserta == "" || tglLahir == "" || blnLahir == "" || thnLahir == "" || kodePTN == "" || namaPTN == "" || kodeProdi == "" || namaProdi == ""
-    ) {
-        alert('Maaf terdapat kesalahan saat mengisi data!, Silahkan ulangi kembali')
-        state = false;
-
-    }else if (namaPeserta == "debugging"){
-        pub_nomorPeserta.textContent = nomorPeserta;
-        pub_namaPeserta.textContent = namaPeserta;
-        pub_tglPeserta.textContent = tglLahir + ' - ' + blnLahir[0].toUpperCase() + blnLahir.slice(1, blnLahir.length) + ' - ' + thnLahir;
-
-        pub_kodeNamaPTN.textContent = kodePTN + ' - ' + namaPTN;
-        pub_kodeNamaProdiTp.textContent = kodeProdi + ' - ' + namaProdi + '  (' + tingkatPendidikan + ')'
-
-        public.style.display = 'grid';
-        settings.style.display = 'none';
-        state = true;
-        body.style.paddinTop = '0';
-        body.style.paddingBottom = '0';
-        body.style.paddingLeft = '0';
-        body.style.paddingRight = '0';
-    } else {
-
-        pub_nomorPeserta.textContent = nomorPeserta;
-        pub_namaPeserta.textContent = namaPeserta;
-        pub_tglPeserta.textContent = tglLahir + ' - ' + blnLahir[0].toUpperCase() + blnLahir.slice(1, blnLahir.length) + ' - ' + thnLahir;
-
-        pub_kodeNamaPTN.textContent = kodePTN + ' - ' + namaPTN;
-        pub_kodeNamaProdiTp.textContent = kodeProdi + ' - ' + namaProdi + '  (' + tingkatPendidikan + ')'
-
-        public.style.display = 'grid';
-        settings.style.display = 'none';
-        state = true;
-        body.style.paddinTop = '0';
-        body.style.paddingBottom = '0';
-        body.style.paddingLeft = '0';
-        body.style.paddingRight = '0';
-
-    }
-
-    if (state) {
-        setTimeout(() => {
-            alert("NB : Untuk kembali ke section sebelumnya klik pada kotak berwana hijau dibagian paling bawah");
-        }, 3000);
-
-    } else {
-        
-    }
-
-}
-
-const reset = document.getElementById('reset');
-
-reset.onclick = () => {
-    window.location.reload()
-}
-
-const button = document.getElementById('button');
-
-button.addEventListener('click', function () {
-    const public = document.getElementById('public');
-
-    const settings = document.getElementById('settings');
-
-    public.style.display = 'none';
-    settings.style.display = 'flex';
-})
->>>>>>> ed8c8304e2bd2d19424949a497b254146048e47f
